86b8a50e27efb82b0e7a610e353f8d3ccb244606
Jeremi Joslin <jeremi23@gmail.com>
2010-03-01 12:58:48 +0800

    upgrade the bookflow app to Grails 1.2.1

--- book-flow/grails-app/conf/Config.groovy
+++ book-flow/grails-app/conf/Config.groovy
@@ -22,3 +22,6 @@ log4j {
 		org.hibernate=false
     }
 }
+// The following properties have been added by the Upgrade process...
+grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
